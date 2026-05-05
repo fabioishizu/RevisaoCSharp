e668db92218090b7b193e31706da26f2f428f92f
Fabio <f.masaoishizu@gmail.com>
2026-05-05 18:03:24 -0300

    Exercicio de membros estaticos

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos - Nelio Alves/TiposBasicoDeDadosCSharp/TiposBasicoDeDadosCSharp/Aulas/Aula_MembrosEstaticos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiposBasicoDeDadosCSharp.Exercicio_aula_membros_estaticos;
+
+namespace TiposBasicoDeDadosCSharp.Aulas
+{
+    public class Aula_MembrosEstaticos : Aula
+    {
+        public Aula_MembrosEstaticos() : base("Membros Estáticos", "Aula sobre membros estáticos em C#")
+        {
+                
+        }
+        public override void Executar()
+        {
+            Console.WriteLine("Qual é a cotação do dólar?");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Quantas dólares você deseja comprar?");
+            double quantidadeDolar = double.Parse(Console.ReadLine());
+            double valorEmReais = ConversorDeMoeda.CalcularValorEmReais(cotacao, quantidadeDolar);
+            Console.WriteLine($"O valor em reais é: {valorEmReais:F2}");
+        }
+    }
+}
---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos - Nelio Alves/TiposBasicoDeDadosCSharp/TiposBasicoDeDadosCSharp/Exercicio_aula_membros_estaticos/ConversorDeMoeda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiposBasicoDeDadosCSharp.Exercicio_aula_membros_estaticos
+{
+    public static class ConversorDeMoeda
+    {
+        private static double IofTaxa = 0.06;
+        public static double CalcularValorEmReais(double cotacao, double quantidadeDolar)
+        {
+            var valorConversaoBruto = (cotacao * quantidadeDolar);
+            var valorIof = valorConversaoBruto * IofTaxa;
+            var total = valorConversaoBruto + valorIof;
+            return total;
+        }
+    }
+}
