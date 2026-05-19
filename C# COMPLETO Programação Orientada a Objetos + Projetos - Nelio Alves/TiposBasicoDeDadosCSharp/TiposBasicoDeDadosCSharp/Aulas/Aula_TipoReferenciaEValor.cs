f4c1a95dfb0b60937203523fb3c307d29c2eaf9e
Fabio <f.masaoishizu@gmail.com>
2026-05-19 20:07:54 -0300

    Aula sobre tipos referencia e tipos valor

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos - Nelio Alves/TiposBasicoDeDadosCSharp/TiposBasicoDeDadosCSharp/Aulas/Aula_TipoReferenciaEValor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiposBasicoDeDadosCSharp.Aulas.Struct;
+
+namespace TiposBasicoDeDadosCSharp.Aulas
+{
+    public class Aula_TipoReferenciaEValor : Aula
+    {
+        public Aula_TipoReferenciaEValor() : base("Tipos de Referência e Valor", "Tipos de referência e valor")
+        {
+                
+        }
+
+        public override void Executar()
+        {
+            Ponto p;
+            p.x = 10;
+            p.y = 20;
+            Console.WriteLine(p);
+
+            p = new Ponto();
+            Console.WriteLine(p);
+        }
+    }
+}
---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos - Nelio Alves/TiposBasicoDeDadosCSharp/TiposBasicoDeDadosCSharp/Aulas/Struct/Ponto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiposBasicoDeDadosCSharp.Aulas.Struct
+{
+    struct Ponto
+    {
+        public double x;
+        public double y;
+
+        public override string ToString()
+        {
+            return $"({x}, {y})";
+        }
+    }
+}
